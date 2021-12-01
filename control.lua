81f63c9afd006cac5bbb00007a6539db7b6caadb
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2021-12-01 10:47:41 -0800

    add support for cooked fish

--- control.lua
+++ control.lua
@@ -4,9 +4,13 @@ script.on_event(defines.events.on_player_used_capsule, function(event)
     local selected = player.selected
     if selected and selected.health then
       selected.health = selected.health + 80
-      -- if player.character.health and player.character.get_health_ratio() < 1 then
-      --   player.character.health = player.character.health - 80
-      -- end
+    end
+  end
+  if event.item.name == "cooked-fish" then
+    local player = game.get_player(event.player_index)
+    local selected = player.selected
+    if selected and selected.health then
+      selected.health = selected.health + 120
     end
   end
 end
