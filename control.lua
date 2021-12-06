effc0840f63f5d08e1cafa72913df4df95aba92a
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2021-12-05 22:33:22 -0800

    0.0.3

--- control.lua
+++ control.lua
@@ -13,5 +13,47 @@ script.on_event(defines.events.on_player_used_capsule, function(event)
       selected.health = selected.health + 120
     end
   end
+  if event.item.name == "raw-cod" then
+    local player = game.get_player(event.player_index)
+    local selected = player.selected
+    if selected and selected.health then
+      selected.health = selected.health + 80
+    end
+  end
+  if event.item.name == "cooked-cod" then
+    local player = game.get_player(event.player_index)
+    local selected = player.selected
+    if selected and selected.health then
+      selected.health = selected.health + 120
+    end
+  end
+  if event.item.name == "raw-salmon" then
+    local player = game.get_player(event.player_index)
+    local selected = player.selected
+    if selected and selected.health then
+      selected.health = selected.health + 80
+    end
+  end
+  if event.item.name == "cooked-salmon" then
+    local player = game.get_player(event.player_index)
+    local selected = player.selected
+    if selected and selected.health then
+      selected.health = selected.health + 120
+    end
+  end
+  if event.item.name == "raw-pufferfish" then
+    local player = game.get_player(event.player_index)
+    local selected = player.selected
+    if selected and selected.health then
+      selected.health = selected.health - 80
+    end
+  end
+  if event.item.name == "raw-clownfish" then
+    local player = game.get_player(event.player_index)
+    local selected = player.selected
+    if selected and selected.health then
+      selected.health = selected.health + 100
+    end
+  end
 end
 )
