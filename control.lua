0757bfee7506d5fc18852577ddc77928bb0a8a8f
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-12-23 00:10:28 -0800

    add support for advanced fishing mod fish

--- control.lua
+++ control.lua
@@ -1,58 +1,32 @@
+local fish_data = {
+  ["raw-fish"] = 80,
+  ["cooked-fish"] = 120,
+
+  ["raw-cod"] = 80,
+  ["raw-salmon"] = 80,
+  ["raw-pufferfish"] = 80,
+  ["raw-clownfish"] = 100,
+
+  ["cooked-cod"] = 120,
+  ["cooked-salmon"] = 80,
+
+  ["af-crab"] = 160,
+  ["af-salmon"] = 80,
+  ["af-tropical"] = 80,
+  ["af-squid"] = 80,
+
+  ["cooked-af-crab"] = 240,
+  ["cooked-af-salmon"] = 80,
+  ["cooked-af-tropical"] = 80,
+  ["cooked-af-squid"] = 80,
+}
+
 script.on_event(defines.events.on_player_used_capsule, function(event)
-  if event.item.name == "raw-fish" then
+  if fish_data[event.item.name] then
     local player = game.get_player(event.player_index)
     local selected = player.selected
     if selected and selected.health then
-      selected.health = selected.health + 80
-    end
-  end
-  if event.item.name == "cooked-fish" then
-    local player = game.get_player(event.player_index)
-    local selected = player.selected
-    if selected and selected.health then
-      selected.health = selected.health + 120
-    end
-  end
-  if event.item.name == "raw-cod" then
-    local player = game.get_player(event.player_index)
-    local selected = player.selected
-    if selected and selected.health then
-      selected.health = selected.health + 80
-    end
-  end
-  if event.item.name == "cooked-cod" then
-    local player = game.get_player(event.player_index)
-    local selected = player.selected
-    if selected and selected.health then
-      selected.health = selected.health + 120
-    end
-  end
-  if event.item.name == "raw-salmon" then
-    local player = game.get_player(event.player_index)
-    local selected = player.selected
-    if selected and selected.health then
-      selected.health = selected.health + 80
-    end
-  end
-  if event.item.name == "cooked-salmon" then
-    local player = game.get_player(event.player_index)
-    local selected = player.selected
-    if selected and selected.health then
-      selected.health = selected.health + 120
-    end
-  end
-  if event.item.name == "raw-pufferfish" then
-    local player = game.get_player(event.player_index)
-    local selected = player.selected
-    if selected and selected.health then
-      selected.health = selected.health - 80
-    end
-  end
-  if event.item.name == "raw-clownfish" then
-    local player = game.get_player(event.player_index)
-    local selected = player.selected
-    if selected and selected.health then
-      selected.health = selected.health + 100
+      selected.health = selected.health + fish_data[event.item.name]
     end
   end
 end
